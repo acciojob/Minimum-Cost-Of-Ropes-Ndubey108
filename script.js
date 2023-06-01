94a3d42938f6d33edb85c751ec6a2fd2baa48df1
Nidhi Dubey <93912780+Ndubey108@users.noreply.github.com>
2023-06-01 12:15:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+ let arr;
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+arr  = document.getElementById("rope-lengths").value.split(',')
+	  let f = 0
+    while(arr.length !== 1){
+        let d = +arr[0] + +arr[1];
+        f+=d
+        arr.shift()
+        arr.shift()
+        arr.unshift(d)
+        arr.sort((a,b)=>a-b)
+    }
+  document.getElementById("result").innerText = f
 }  
